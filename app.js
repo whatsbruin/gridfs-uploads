152df30bff1344369ee29854b658e88f9575735d
JR Dexter <95322247+jr-dexter@users.noreply.github.com>
2023-11-21 22:29:36 -0500

    add filter to filetype before upload

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const { GridFsStorage } = require("multer-gridfs-storage");
 require('dotenv').config();
 const Grid = require("gridfs-stream")
 const methodOverride = require('method-override');
-//
 const Contact = require('./models/contact');
 //
 const app = express();
@@ -25,55 +24,62 @@ app.use(methodOverride('_method'));
 mongoose.set('strictQuery', true);
 
 const mongoURI = process.env.DATABASE_URL;
-const db = mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const conn = mongoose.createConnection(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 let gfs;
 const bucket_name = "images"
-conn.once("open", () => {
+const db = mongoose.connection;
+db.once('open', () => {
   // init stream
-  gfs = Grid(conn.db, mongoose.mongo)
+  gfs = Grid(db, mongoose.mongo)
   gfs.collection(bucket_name)
-  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+  gridfsBucket = new mongoose.mongo.GridFSBucket(db, {
     bucketName: bucket_name,
   });
-})
+});
 
-function isValidFile(file) {
-  if (file.mimetype === 'image/png'
-    || file.mimetype == "image/jpg"
-    || file.mimetype == "image/jpeg") {
-    return true
-  } else {
-    return false
+// Handle connection errors
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+
+function isValidImage(file) {
+  // Check file size (64MB = 64 * 1024 * 1024 bytes)
+  const maxSize = 64 * 1024 * 1024; // 64MB
+  if (file.size > maxSize) {
+    console.log('File exceeds the maximum size limit (64MB)');
+    return false;
   }
+  // Validate the file type
+  if (!file.mimetype.startsWith('image')) {
+    return false;
+  };
+  return true
 }
+
 // Storage
 const storage = new GridFsStorage({
   url: mongoURI,
   file: (req, file) => {
-    if (isValidFile(file)) {
-      // Generate a unique file name, you can use any method to generate it.
-      // In this example we are using Date.now() to generate unique name
-      const filename = `${Date.now()}_${file.originalname}`;
-      // Create an object containing the file information
-      // It will be used by multer-gridfs-storage to save the file in MongoDB
-      const fileInfo = {
-        filename: filename,
-        bucketName: bucket_name // specify the bucket name (change this and multer instanct to change which collection.files it saves to in Mongodb)
-      };
-      return fileInfo;
+    if (!isValidImage(file)) {
+      return;
     }
+    // Generate a unique file name, you can use any method to generate it.
+    // In this example we are using Date.now() to generate unique name
+    const filename = `${Date.now()}_${file.originalname}`;
+    // Create an object containing the file information
+    // It will be used by multer-gridfs-storage to save the file in MongoDB
+    const fileInfo = {
+      filename: filename,
+      bucketName: bucket_name // specify the bucket name (change this and multer instanct to change which collection.files it saves to in Mongodb)
+    };
+    return fileInfo;
   }
-
-});
+})
 
 const upload = multer({ storage });
 
@@ -120,6 +126,7 @@ app.post('/', upload.single('image'), (req, res, next) => {
   res.redirect("/")
 });
 
+
 app.delete('/delete/:fileID', async (req, res) => {
 
   const obj_id = new mongoose.Types.ObjectId(req.params.fileID);
@@ -144,9 +151,35 @@ app.get('/contacts', async (req, res) => {
   // await cursor.close();
 
 });
+app.get('/contactsall', async (req, res) => {
+  let contact = await Contact.find({})
+  // const cursor = gfs.files.find({});
+  // const allFiles = await cursor.toArray();
+  // res.render("showContacts", {
+  //   contact: contact,
+  // })
+  res.json(contact)
+  // await cursor.close();
 
+});
 app.post("/contactForm", upload.single('image'), async (req, res) => {
-  // add contact
+  // const filter = { _id: ObjectId(userId) }; // Filter for a specific ObjectId
+  // const filter = { name: req.body.contactName }
+  // const update = { photo: req.body.filename }; // Update the 'age' field to 30
+
+  // Contact.updateOne(filter, update)
+  //   .then((result) => {
+  //     console.log('Update result:', result);
+  //     upload.single('image')
+  //     // 'result' will contain information about the update operation
+  //     // result.nModified will be 1 if a document matched the filter and was updated
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error updating document:', error);
+  //   });
+
+  console.log(JSON.stringify(req.body));
+
   const newContact = await Contact.create({
     name: req.body.contactName,
     email: req.body.contactEmail,
